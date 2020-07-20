fdf2cd79d0f4a0a47f1d89e2c9dca32c2562d9da
Diana Surovtseva <dia.surovtseva@gmail.com>
2020-07-20 22:15:16 +0300

    DataProvider added

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,6 +2,9 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class LoginPage {
 
@@ -28,5 +31,12 @@ public class LoginPage {
     }
     public void navigateTo(){
         driver.get("https://jira.hillel.it/secure/Dashboard.jspa");
+    }
+    public boolean errorMessageIsPresent(String message){
+        return driver.findElement(By.xpath("//*[contains(text(),'" + message + "')]")).isDisplayed();
+    }
+    public void errorMessageIsDisplayed(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(presenceOfElementLocated(By.id("usernameerror"))).isDisplayed();
     }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
@@ -23,6 +24,13 @@ public class LoginTest {
         loginPage = new LoginPage(driver);
         homePage = new HomePage(driver);
     }
+    @DataProvider(name = "Logins")
+    public Object [][]createData1(){
+        return new Object[][]{
+                {"WrongName","DianaSurovtseva","Sorry, your username and password are incorrect - please try again."},
+                {"DianaSurovtseva","WrongPassword","Sorry, your username and password are incorrect - please try again."},
+        };
+    }
 
         @Test
         public void successfulLoginTest () {
@@ -34,6 +42,17 @@ public class LoginTest {
 
             assertTrue(homePage.userIconIsPresent());
         }
+    @Test(dataProvider = "Logins")
+    public void unSuccessfulLoginTest (String name, String password, String expectedResult) throws InterruptedException {
+        homePage.navigateTo();
+        loginPage.enterUserName(name);
+        loginPage.setUserPassInput(password);
+        loginPage.clickLogBut();
+        loginPage.errorMessageIsDisplayed();
+
+
+        assertTrue(loginPage.errorMessageIsPresent(expectedResult));
+    }
 
         @Test
         public void failedTest () {
