9d0ef1211b699aab1521a548978d447f7208c02d
Surovtseva Diana <dia.surovtseva@gmail.com>
2020-08-27 16:09:03 +0300

    refactoring assertCreateWindowNotPresent method

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -71,7 +71,6 @@ public class HomePage {
     public void assertCreateWindowNotPresent() {
         try {
             driver.findElement(createIssueTitle);
-            fail();
         } catch (NoSuchElementException ex) {
             System.out.println("CreateIssueWindow isn't displayed");
         }
